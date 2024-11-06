a19e83ec5d5a8f3ddbe67ac9e1466a08671c4ea8
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2024-11-06 15:02:03 -0300

    atualizacao pagina instalações

---
+++ app.js
@@ -0,0 +1,12 @@
+var myImage=document.querySelector("#imageid");
+function quadraImage()
+{
+    
+    myImage.src="imagens/quadra.jpg";
+
+}
+function salaImage()
+{
+        myImage.src="imagens/salafund.jpg";
+
+}
