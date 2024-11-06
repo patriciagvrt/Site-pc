0b601616a37edde5fed4abf799e84f72d897d74f
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2024-11-06 16:28:18 -0300

    inclusao de mais fotos

--- app.js
+++ app.js
@@ -7,6 +7,31 @@ function quadraImage()
 }
 function salaImage()
 {
-        myImage.src="imagens/salafund.jpg";
+    myImage.src="imagens/salafund.jpg";
 
+}
+
+function playgroundImage()
+{
+    myImage.src="imagens/brinquedoedinfantil.jpg";
+}
+function hortaImage()
+{
+    myImage.src="imagens/salafund.jpg";
+}
+function monitoramentoImage()
+{
+    myImage.src="imagens/monitoramento.jpg";
+}
+function labImage()
+{
+    myImage.src="imagens/salafund.jpg";
+}
+function balletImage()
+{
+    myImage.src="imagens/salafund.jpg";
+}
+function leituraImage()
+{
+    myImage.src="imagens/integral.jpg";
 }
