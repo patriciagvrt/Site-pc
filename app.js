44bd0d0d8a20d5a72dd3a11ee8c310f5b2622908
patriciagivort <63522907+patriciagvrt@users.noreply.github.com>
2024-11-07 12:57:13 -0300

    Atualizacao fotos

--- app.js
+++ app.js
@@ -2,36 +2,36 @@ var myImage=document.querySelector("#imageid");
 function quadraImage()
 {
     
-    myImage.src="imagens/quadra.jpg";
+    myImage.src="imagens/ginasio.png";
 
 }
 function salaImage()
 {
-    myImage.src="imagens/salafund.jpg";
+    myImage.src="imagens/salas.png";
 
 }
 
 function playgroundImage()
 {
-    myImage.src="imagens/brinquedoedinfantil.jpg";
+    myImage.src="imagens/playground.png";
 }
 function hortaImage()
 {
-    myImage.src="imagens/salafund.jpg";
+    myImage.src="imagens/horta.png";
 }
 function monitoramentoImage()
 {
-    myImage.src="imagens/monitoramento.jpg";
+    myImage.src="imagens/monitoramento.png";
 }
 function labImage()
 {
-    myImage.src="imagens/salafund.jpg";
+    myImage.src="imagens/laboratorio.png";
 }
 function balletImage()
 {
-    myImage.src="imagens/salafund.jpg";
+    myImage.src="imagens/ballet.png";
 }
 function leituraImage()
 {
-    myImage.src="imagens/integral.jpg";
+    myImage.src="imagens/integral.png";
 }
